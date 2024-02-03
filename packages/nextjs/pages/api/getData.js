80621f3ccb0c02583599833b0819affc686283e4
electrone901 <Carbajalluis@rocketmail.com>
2024-02-03 17:26:49 -0500

    code cleanup

--- packages/nextjs/pages/api/getData.js
+++ packages/nextjs/pages/api/getData.js
@@ -1,11 +1,11 @@
 import fs from "fs";
-import path from "path";
 import csv from "csv-parser";
 
 export default async function handler(req, res) {
   const {
     query: { id },
   } = req;
+
   try {
     const project = await getProjectById(id);
 
@@ -18,24 +18,6 @@ export default async function handler(req, res) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
-
-  // const filePath = path.join(process.cwd(), "public", "csv/RetroPGF3 Results - ConsolidatedData.csv");
-  // try {
-  //   const data = await new Promise((resolve, reject) => {
-  //     const results = [];
-  //     fs.createReadStream(filePath)
-  //       .pipe(csv())
-  //       .on("data", row => results.push(row))
-  //       .on("end", () => resolve(results))
-  //       .on("error", error => reject(error));
-  //   });
-
-  //   console.log("🚀 ~ data_____:", data);
-  //   res.status(200).json(data);
-  // } catch (error) {
-  //   console.error("Error reading CSV file:", error);
-  //   res.status(500).send("Internal Server Error");
-  // }
 }
 
 async function getProjectById(id) {
