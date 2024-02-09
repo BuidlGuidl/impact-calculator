4afa85a6caae7741900a3e2e93da0fdf87a8f452
escottalexander@gmail.com <escottalexander@gmail.com>
2024-02-08 21:24:46 -0500

    add impact endpoint for graph

---
+++ packages/nextjs/app/types/data.ts
@@ -0,0 +1,66 @@
+export interface RetroPGF3Results extends ImpactVectors, Metadata {
+  Project_ID: string;
+  "Result: # Ballots": number;
+  "Result: Median OP": number;
+  "Result: Quorum Reached": boolean;
+  "Result: Received OP": number;
+  "Funding: Governance Fund": number;
+  "Funding: Other": number;
+  "Funding: Partner Fund": number;
+  "Funding: RPGF1": number;
+  "Funding: RPGF2": number;
+  "Funding: Revenue": number;
+  "Keywords: Base": number;
+  "Keywords: Farcaster": number;
+  "Keywords: Zora": number;
+  "Link: Contract on Base": number;
+  "Link: Contract on OP Mainnet": number;
+  "Link: Dune": number;
+  "Link: Flipside": number;
+  "Link: GitHub": number;
+  "Link: GitHub (duneanalytics)": number;
+  "Link: GitHub (ethereum)": number;
+  "Link: GitHub (ethereum-optimism)": number;
+  "Link: NPM Package": number;
+  "Link: Optimism Gov": number;
+  "Link: Substack": number;
+  "Link: Twitter": number;
+  "GTP: VC Funding Amount"?: number;
+  "GTP: Has Token"?: boolean;
+  "GTP: Has VC Funding": number;
+}
+
+export interface ImpactVectors {
+  "OSO: # GitHub Repos"?: number;
+  "OSO: Date First Commit": string;
+  "OSO: Total Stars"?: number;
+  "OSO: Total Forks"?: number;
+  "OSO: Total Contributors"?: number;
+  "OSO: Contributors Last 6 Months"?: number;
+  "OSO: Avg Monthly Active Devs Last 6 Months": number;
+  "OSO: # OP Contracts"?: number;
+  "OSO: Date First Txn": string;
+  "OSO: Total Onchain Users"?: number;
+  "OSO: Onchain Users Last 6 Months"?: number;
+  "OSO: Total Txns"?: number;
+  "OSO: Total Txn Fees (ETH)": number;
+  "OSO: Txn Fees Last 6 Months (ETH)": number;
+  "OSO: # NPM Packages"?: number;
+  "OSO: Date First Download": string;
+  "OSO: Total Downloads"?: number;
+  "OSO: Downloads Last 6 Months"?: number;
+  "OSO: Has Profile": boolean;
+}
+
+export interface Metadata {
+  "Meta: Project Name": string;
+  "Meta: Project Image": string;
+  "Meta: Applicant Type": string;
+  "Meta: Website": string;
+  "Meta: Bio": string;
+  "Meta: Payout Address": string;
+  "Category: Collective Governance": number;
+  "Category: Developer Ecosystem": number;
+  "Category: End User Experience and Adoption": number;
+  "Category: OP Stack": number;
+}
