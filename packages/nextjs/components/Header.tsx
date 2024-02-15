1c26780d148e5c89c4423464fda2dc7dd297b892
KcPele <fidekg123@gmail.com>
2024-02-15 19:21:18 +0100

    header title changed

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -93,7 +93,7 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
+            <span className="font-bold leading-tight">Impact Calculator</span>
             <span className="text-xs">Ethereum dev stack</span>
           </div>
         </Link>
