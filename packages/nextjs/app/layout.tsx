95f4c3414815de6a39545c6bd914c629d4d346bb
Sam Wellander <samwellander@gmail.com>
2024-02-19 11:38:00 -0800

    re-wrap app with scaffold providers

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
 import "~~/styles/globals.css";
 
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
@@ -42,7 +43,9 @@ export const metadata: Metadata = {
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
+      </body>
     </html>
   );
 };
