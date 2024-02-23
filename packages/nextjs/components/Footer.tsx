a90d5362b582eb8881261d65fd00ade48b46eeb8
EngrGord <chijokeogbuanya@gmail.com>
2024-02-23 09:50:36 +0100

    Added the repo link to the fork me ancor.

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -44,7 +44,12 @@ export const Footer = () => {
         <ul className="menu menu-horizontal w-full">
           <div className="flex justify-center items-center gap-2 text-sm w-full">
             <div className="text-center">
-              <a href="https://github.com/scaffold-eth/se-2" target="_blank" rel="noreferrer" className="link">
+              <a
+                href="https://github.com/BuidlGuidl/impact-calculator"
+                target="_blank"
+                rel="noreferrer"
+                className="link"
+              >
                 Fork me
               </a>
             </div>
