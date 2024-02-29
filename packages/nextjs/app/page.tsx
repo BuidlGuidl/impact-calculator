acc225d0e1a0b642cbd6a3985bef9feb98262cfd
EngrGord <chijokeogbuanya@gmail.com>
2024-02-29 12:05:07 +0100

    added resizing to table and vector selection.

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -61,10 +61,8 @@ const Home: NextPage = () => {
   }, [selectedVectors]);
 
   return (
-    <main
-      className={`w-full flex flex-col gap-6 sm:gap-10 b-md:flex-row p-3 ${!fullGraph && "lg:mb-[22rem]"} relative`}
-    >
-      <div className={`${fullGraph ? "w-full" : "lg:w-[55%]"} duration-400 ease-in-out h-[60vh] transition-all`}>
+    <main className={`w-full flex flex-col gap-6 sm:gap-10 p-3 ${!fullGraph && "lg:mb-[22rem]"} relative`}>
+      <div className={`${fullGraph ? "w-full" : "lg:w-[58%]"} duration-500 ease-in-out h-[60vh] transition-all`}>
         <h2 className="text-center">Impact Calculator 🌱</h2>
         <div className="flex w-full h-[60vh]">
           {impactData.length > 0 && (
@@ -74,10 +72,14 @@ const Home: NextPage = () => {
       </div>
 
       <div className="flex flex-col lg:flex-row">
-        <div className="mx-5">
+        <div className={`lg:mx-5 ${fullGraph ? "flex-1" : "lg:w-[58%]"} w-full lg:mt-0 mt-6`}>
           <ImpactVectorTable />
         </div>
-        <div className="max-h-[100dvh] overflow-hidden b-md:max-w-[34rem] w-full rounded-3xl p-6 border grid gap-6 mx-auto">
+        <div
+          className={`b-md:w-[34rem] w-full ${
+            fullGraph ? "lg:relative mt-0" : "lg:absolute lg:top-0 lg:right-0"
+          } transition-all overflow-hidden h-auto b-md:max-w-[34rem] rounded-3xl p-6 border grid gap-6 mx-auto duration-1000 ease-in-out`}
+        >
           <div className="rounded-xl grid grid-cols-2 bg-base-300 p-1">
             <button className={` bg-base-100 font-semibold  py-3 text- rounded-xl text-center w-full`}>
               Impact Vectors
