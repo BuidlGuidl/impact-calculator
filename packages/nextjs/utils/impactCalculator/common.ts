d683ac6299804ebfbe89c2bd194a48619d35c291
Sam Wellander <samwellander@gmail.com>
2024-03-06 06:50:17 -0800

    update number abbreviation function
    to handle millions

--- packages/nextjs/utils/impactCalculator/common.ts
+++ packages/nextjs/utils/impactCalculator/common.ts
@@ -1,4 +1,7 @@
 export const abbreviateNumber = (num: number) => {
+  if (num >= 1000000) {
+    return Number((num / 1000000).toFixed(2)) + "m";
+  }
   if (num >= 1000) {
     return Math.round(num / 1000) + "k";
   } else {
