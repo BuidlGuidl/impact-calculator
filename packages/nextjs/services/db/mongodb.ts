07153f5c0a2d3257d8215962e79cec7fd028e8b7
Sam Wellander <samwellander@gmail.com>
2024-03-12 12:10:45 -0700

    remove mongodb doc comments

--- packages/nextjs/services/db/mongodb.ts
+++ packages/nextjs/services/db/mongodb.ts
@@ -11,8 +11,6 @@ let client;
 let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
   const globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>;
   };
@@ -23,11 +21,8 @@ if (process.env.NODE_ENV === "development") {
   }
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
-  // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
 export default clientPromise;
