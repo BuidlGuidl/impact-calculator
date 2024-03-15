1e86e6e4f67e2c3ba1ba3c4fdd9528d059350ea6
EngrGord <chijokeogbuanya@gmail.com>
2024-03-15 12:44:40 +0100

    added scaffoldEthApp to return header and footer

--- packages/nextjs/components/ScaffoldEthProviders.tsx
+++ packages/nextjs/components/ScaffoldEthProviders.tsx
@@ -1,28 +1,47 @@
 "use client";
 
-import { Footer } from "./Footer";
-import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit";
+import { useEffect } from "react";
+import { RainbowKitProvider, lightTheme } from "@rainbow-me/rainbowkit";
+import { Toaster } from "react-hot-toast";
 import { WagmiConfig } from "wagmi";
+import { Footer } from "~~/components/Footer";
+import { Header } from "~~/components/Header";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
 import { ProgressBar } from "~~/components/scaffold-eth/ProgressBar";
-import { useDarkMode } from "~~/hooks/scaffold-eth/useDarkMode";
+import { useNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
+import { useGlobalState } from "~~/services/store/store";
 import { wagmiConfig } from "~~/services/web3/wagmiConfig";
 import { appChains } from "~~/services/web3/wagmiConnectors";
 
-export const ScaffoldEthProviders = ({ children }: { children: React.ReactNode }) => {
-  const { isDarkMode } = useDarkMode();
+const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
+  const price = useNativeCurrencyPrice();
+  const setNativeCurrencyPrice = useGlobalState(state => state.setNativeCurrencyPrice);
 
+  useEffect(() => {
+    if (price > 0) {
+      setNativeCurrencyPrice(price);
+    }
+  }, [setNativeCurrencyPrice, price]);
+
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="relative flex flex-col flex-1">{children}</main>
+        <Footer />
+      </div>
+      <Toaster />
+    </>
+  );
+};
+
+export const ScaffoldEthProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <WagmiConfig config={wagmiConfig}>
       <ProgressBar />
-      <RainbowKitProvider
-        chains={appChains.chains}
-        avatar={BlockieAvatar}
-        theme={isDarkMode ? darkTheme() : lightTheme()}
-      >
-        {children}
+      <RainbowKitProvider chains={appChains.chains} avatar={BlockieAvatar} theme={lightTheme()}>
+        <ScaffoldEthApp>{children}</ScaffoldEthApp>
       </RainbowKitProvider>
-      <Footer />
     </WagmiConfig>
   );
 };
