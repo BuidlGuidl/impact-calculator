07f4cafd382333c18acccb45be8a41204928b649
EngrGord <chijokeogbuanya@gmail.com>
2024-03-16 08:08:12 +0100

    added nav magin

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -8,12 +8,10 @@ import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaff
  */
 export const Header = () => {
   return (
-    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 z-20 shadow-md shadow-secondary px-0 sm:px-2">
-      <div className="md:navbar-end text-right items-center lg:w-1/2">
+    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 z-20 mb-2 shadow-md shadow-secondary px-0 sm:px-2">
+      <div className="md:navbar-end hidden md:flex text-right items-center lg:w-1/2">
         <Link href="/" passHref className="flex items-center gap-2 ml-4 mr-6 shrink-0">
-          <div className="hidden md:flex flex-col">
-            <h2 className="text-right">Impact Calculator 🌱</h2>
-          </div>
+          <h2 className="text-right">Impact Calculator 🌱</h2>
         </Link>
       </div>
       <div className="navbar-end flex-grow mr-4">
