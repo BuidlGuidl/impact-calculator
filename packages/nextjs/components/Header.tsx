3a71c132889c02cc101c992b0178cec2a8113150
EngrGord <chijokeogbuanya@gmail.com>
2024-03-16 12:44:43 +0100

    removed navbar shadow and margin

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -8,7 +8,7 @@ import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaff
  */
 export const Header = () => {
   return (
-    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 z-20 mb-2 shadow-md shadow-secondary px-0 sm:px-2">
+    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 z-20 px-0 sm:px-2">
       <div className="md:navbar-end hidden md:flex text-right items-center lg:w-1/2">
         <Link href="/" passHref className="flex items-center gap-2 ml-4 mr-6 shrink-0">
           <h2 className="text-right">Impact Calculator 🌱</h2>
