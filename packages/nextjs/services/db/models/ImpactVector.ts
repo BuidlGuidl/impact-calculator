81253360a9a874a6d5c34a0ff40e170c13ee8c07
Sam Wellander <samwellander@gmail.com>
2024-04-10 14:38:24 -0700

    add models and schemas

---
+++ packages/nextjs/services/db/models/ImpactVector.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { Vector } from "~~/app/types/data";
+
+const ImpactVectorSchema = new mongoose.Schema<Vector>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  sourceName: {
+    type: String,
+    required: true,
+  },
+  parent: {
+    type: String,
+    required: true,
+  },
+  fieldName: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.models.ImpactVector || mongoose.model<Vector>("ImpactVector", ImpactVectorSchema);
